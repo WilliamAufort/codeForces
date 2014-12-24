5f0589e741c43d1b9d70e869e18eb1884f624c2f
William Aufort <william.aufort@ens-lyon.fr>
2014-12-24 10:44:26 +0100

    Digital Counter problem

---
+++ digitalCounter.cpp
@@ -0,0 +1,57 @@
+/****************************************\
+| A. Digital Counter					 |
+| time limit per test :1 second			 |
+| memory limit per test : 256 megabytes  |
+| input : standard input				 |
+| output : standard output				 |
+\****************************************/
+
+/*
+
+Malek lives in an apartment block with 100 floors numbered from 0 to 99. The apartment has an elevator with a digital counter showing the floor that the elevator is currently on. The elevator shows each digit of a number with 7 light sticks by turning them on or off. The picture below shows how the elevator shows each digit.
+
+One day when Malek wanted to go from floor 88 to floor 0 using the elevator he noticed that the counter shows number 89 instead of 88. Then when the elevator started moving the number on the counter changed to 87. After a little thinking Malek came to the conclusion that there is only one explanation for this: One of the sticks of the counter was broken. Later that day Malek was thinking about the broken stick and suddenly he came up with the following problem.
+
+Suppose the digital counter is showing number n. Malek calls an integer x (0 ≤ x ≤ 99) good if it's possible that the digital counter was supposed to show x but because of some(possibly none) broken sticks it's showing n instead. Malek wants to know number of good integers for a specific n. So you must write a program that calculates this number. Please note that the counter always shows two digits.
+Input
+
+The only line of input contains exactly two digits representing number n (0 ≤ n ≤ 99). Note that n may have a leading zero.
+Output
+
+In the only line of the output print the number of good integers.
+
+*/
+
+#include <iostream>
+#include <utility> // pair
+#include <sstream>
+
+using namespace std;
+
+int stringToInt( std::string word ) {
+  int number ;
+  std::istringstream(word) >> number ; 
+  return number ;
+}
+
+int getInput (void) {
+  string ligne;
+  getline(cin,ligne);
+  return stringToInt(ligne) ;
+}
+
+pair<int,int> getTwoNumber(int input) {
+	int n1 = input / 10;
+	int n2 = input - 10 * n1 ;
+	return pair<int,int>(n1,n2);
+}
+
+
+int main (int argc, char* argv[]) {
+	pair<int,int> inputs = getTwoNumber(getInput());
+	int table[10] = {2,7,2,3,3,4,2,5,1,2} ;
+	cout << table[inputs.first]*table[inputs.second] << endl;
+	return 0;
+}
+
+
